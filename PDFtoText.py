3ec09155d119f0579832b8844ff83fdb5b74f227
yineza7 <157069735+yineza7@users.noreply.github.com>
2024-04-01 14:21:32 -0500

    Update PDFtoText.py

--- PDFtoText.py
+++ PDFtoText.py
@@ -3,13 +3,22 @@
 
 from pdfquery import PDFQuery
 
+
+#this is where you can add the .pdf that needs to be converted
 usrin = PDFQuery("Mustafa-Syllabus.pdf")
 usrin.load()
 
 text_elements = usrin.pq('LTTextLineHorizontal')
 
-text = [t.text for t in text_elements]
+'''text = [t.text for t in text_elements]
 
 print(text)
+'''
 
+text = [t.text for t in text_elements]
 
+# Concatenate text elements into a single string
+text_string = '\n'.join(text)
+
+# Print the resulting text string
+print(text_string)
